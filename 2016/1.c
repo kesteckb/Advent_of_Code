e5d1eb2bde3f9eaf3f9fe4ed4f427191fe5c6b4b
Kyle Steckbauer <kesteckb@gmail.com>
2020-12-03 19:01:19 +0000

    day 3 complete

--- 2016/1.c
+++ 2016/1.c
@@ -1,21 +1,94 @@
 #include <stdio.h>
-#include <stlib.h>
+#include <stdlib.h>
+
+void part_1(char facing, char turn, int blocks, int *move_x, int *move_y);
 
 int main(void)
 {
-    char move;
+    FILE *fptr;
+    char turn;
     int blocks;
+    char facing;
+    int move_x = 0;
+    int move_y = 0;
 
-    move = 'L';
+    /* temporarily hard coded */
+    facing = 'N';
+    turn = 'L';
     blocks = 5;
 
-    navigate(move, blocks);
+    fptr = fopen("input1.txt", "r");
+
+    
+
+    part_1(facing, turn, blocks, &move_x, &move_y);
+
+    printf("Part 1: %d blocks\n", (abs(move_x) + (abs(move_y))));
+
+    fclose(fptr);
+    return 0;
 }
 
-void navigate(char move, int blocks)
+void part_1(char facing, char turn, int blocks, int *move_x, int *move_y)
 {
-    switch (char)move {
-        case 'N':
-        
+    if (facing == 'N')
+    {
+        if (turn == 'L')
+        {
+            facing = 'W';
+            (*move_x) -= blocks;
+            return;
+        }
+        else if (turn == 'R')
+        {
+            facing = 'E';
+            (*move_x) += blocks;
+            return;
+        }
+    }
+    else if (facing == 'E')
+    {
+        if (turn == 'L')
+        {
+            facing = 'N';
+            (*move_y) += blocks;
+            return;
+        }
+        else if (turn == 'R')
+        {
+            facing = 'S';
+            (*move_y) -= blocks;
+            return;
+        }
+    }
+    else if (facing == 'S')
+    {
+        if (turn == 'L')
+        {
+            facing = 'E';
+            (*move_x) += blocks;
+            return;
+        }
+        else if (turn == 'R')
+        {
+            facing = 'W';
+            (*move_x) -= blocks;
+            return;
+        }
+    }
+    else
+    {
+        if (turn == 'L')
+        {
+            facing = 'S';
+            (*move_y) -= blocks;
+            return;
+        }
+        else if (turn == 'R')
+        {
+            facing = 'N';
+            (*move_y) += blocks;
+            return;
+        }
     }
 }
--- 2020/3.c
+++ 2020/3.c
@@ -1,14 +1,28 @@
-/*
-
-*/
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define MAX_PATH 50
 
 typedef enum bool {false,true} bool;
+
+void count_trees11(char trees[], int row, int *conflicts);
+void count_trees31(char trees[], int row, int *conflicts);
+void count_trees51(char trees[], int row, int *conflicts);
+void count_trees71(char trees[], int row, int *conflicts);
+void count_trees12(char trees[], int row, int *conflicts);
 
 int main(void)
 {
     FILE *fptr;
+    char buffer[MAX_PATH];
+    int trees11 = 0;
+    int trees31 = 0;
+    int trees51 = 0;
+    int trees71 = 0;
+    int trees12 = 0;
+    int count = 0;
+    unsigned long total;
 
 
     fptr = fopen("input3.txt", "r");
@@ -18,7 +32,62 @@ int main(void)
         exit(EXIT_FAILURE);
     }
 
+    while (fgets(buffer, MAX_PATH, fptr))
+    {
+        count_trees11(buffer, count, &trees11);
+        count_trees31(buffer, count, &trees31);
+        count_trees51(buffer, count, &trees51);
+        count_trees71(buffer, count, &trees71);
+        count_trees12(buffer, count, &trees12);
+        count++;
+    }
+    total = (unsigned long)trees11 * (unsigned long)trees31 *
+        (unsigned long)trees51 * (unsigned long)trees71 * (unsigned long)trees12;
 
+    printf("Part 1: %d\nPart 2: %ld\n", trees31, total);
     fclose(fptr);
     return 0;
+}
+
+void count_trees31(char trees[], int row, int *conflicts)
+{
+    if (trees[(3 * row) % 31] == '#')
+    {
+        (*conflicts)++;
+    }
+}
+
+void count_trees11(char trees[], int row, int *conflicts)
+{
+    if (trees[(row) % 31] == '#')
+    {
+        (*conflicts)++;
+    }
+}
+
+void count_trees51(char trees[], int row, int *conflicts)
+{
+    if (trees[(5 * row) % 31] == '#')
+    {
+        (*conflicts)++;
+    }
+}
+
+void count_trees71(char trees[], int row, int *conflicts)
+{
+    if (trees[(7 * row) % 31] == '#')
+    {
+        (*conflicts)++;
+    }
+}
+
+void count_trees12(char trees[], int row, int *conflicts)
+{
+    if (row % 2 == 0)
+    {
+        if (trees[(row/2) % 31] == '#')
+        {
+            (*conflicts)++;
+        }
+    }
 }
